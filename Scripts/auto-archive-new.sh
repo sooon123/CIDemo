c969fd7c75297e52116847785eef5b3bc6a30887
songhe <songxianhehe@163.com>
2018-12-01 12:56:56 +0800

    fix 2

---
+++ Scripts/auto-archive-new.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CONFIGURATION=$1
+
+if [ -z "${CONFIGURATION}" ]; then
+    echo -e "No configuration specified."
+    exit 255;
+fi
+
+EXPORT_PATH="./build/${CONFIGURATION}-build"
+
+rm -rf "${EXPORT_PATH}"
+
+buildNumber=`date +"%Y%m%d%H%M"`
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "../CIDemo/Info.plist"
+
+xcrun xcodebuild -workspace ../CIDemo.xcworkspace/ -configuration "${CONFIGURATION}" -scheme "CIDemo${CONFIGURATION}" clean archive -archivePath "${EXPORT_PATH}/ipa" -verbose CODE_SIGN_IDENTITY="iPhone Distribution: Beijing Zhongguancun Ronghui Financial Information Service Co., Ltd" APP_PROFILE="InHouse - CIDemo"
+
+xcrun xcodebuild -exportArchive -exportOptionsPlist InHouse-QA.plist -archivePath "${EXPORT_PATH}/ipa.xcarchive" -exportPath "${EXPORT_PATH}"
