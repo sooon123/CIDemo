c9bb86f23cd56bd8adfe7f4d611dd87e9f49ee9c
songhe <songxianhehe@163.com>
2018-12-04 11:39:34 +0800

    fix 33

--- Scripts/auto-archive-new.sh
+++ Scripts/auto-archive-new.sh
@@ -2,6 +2,10 @@
 
 CONFIGURATION=$1
 
+#CODE_SIGN_IDENTITY="iPhone Distribution: Beijing Zhongguancun Ronghui Financial Information Service Co., Ltd"
+#APP_PROFILE="InHouse - CIDemo"
+
+
 if [ -z "${CONFIGURATION}" ]; then
     echo -e "No configuration specified."
     exit 255;
@@ -13,12 +17,11 @@ EXPORT_PATH="./build/${CONFIGURATION}-build"
 
 rm -rf "${EXPORT_PATH}"
 #mkdir -p "build/${CONFIGURATION}-build"
-security unlock-keychain login.keychain
- security unlock-keychain -p " " "/Users/sh/Library/Keychains/login.keychain-db"
+#  security unlock-keychain -p " " "/Users/sh/Library/Keychains/login.keychain-db"
 
 buildNumber=`date +"%Y%m%d%H%M"`
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "../CIDemo/Info.plist"
 
-xcrun xcodebuild -workspace ../CIDemo.xcworkspace/ -configuration "${CONFIGURATION}" -scheme "CIDemo${CONFIGURATION}" clean archive -archivePath "${EXPORT_PATH}/ipa" -verbose CODE_SIGN_IDENTITY="iPhone Distribution: Beijing Zhongguancun Ronghui Financial Information Service Co., Ltd" APP_PROFILE="InHouse - CIDemo"
+xcrun xcodebuild -workspace ../CIDemo.xcworkspace/ -configuration "${CONFIGURATION}" -scheme "CIDemo${CONFIGURATION}" clean archive -archivePath "${EXPORT_PATH}/ipa" -verbose
 
-# xcrun xcodebuild -exportArchive -exportOptionsPlist InHouse-QA.plist -archivePath "${EXPORT_PATH}/ipa.xcarchive" -exportPath "${EXPORT_PATH}"
+ xcrun xcodebuild -exportArchive -exportOptionsPlist InHouse-QA.plist -archivePath "${EXPORT_PATH}/ipa.xcarchive" -exportPath "${EXPORT_PATH}"
